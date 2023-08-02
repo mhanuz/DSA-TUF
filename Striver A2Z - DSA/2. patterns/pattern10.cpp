952ea859997b06e7f6b6f67557a42e8a1152e858
Anuz <ceanuz66@gmail.com>
2023-08-02 14:33:55 +0600

    find out symmatric: trick of symmatric matrix

--- Striver A2Z - DSA/2. patterns/pattern10.cpp
+++ Striver A2Z - DSA/2. patterns/pattern10.cpp
@@ -1,25 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void get_pattern(int num){
-	for(int i=1; i<=num; i++)
+void get_pattern(int num)
+{
+	int starts;
+	for(int i=1; i<=2*num-1; i++)
 	{
-		for(int j=1; j<=i; j++)
+		starts = i;
+		if(i > num) starts = 2*num - i;
+		for(int j=1; j<=starts; j++)
 		{
-			cout << "*" << " ";
+			cout << "*";
 		}
+		
 		cout << endl;
 	}
-	for(int i=1; i<num; i++)
-	{
-		for(int j=1; j<=num-i; j++)
-		{
-			cout << "*" << " ";
-		}
-		cout << endl;
-	}
-}
 
+}
 int main()
 {
 	int number;
