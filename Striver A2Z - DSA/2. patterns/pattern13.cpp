30e29a550a213a40cae380cfb4549280344f7942
Anuz <ceanuz66@gmail.com>
2023-08-06 08:17:09 +0600

    update pattern 13: update the logic

--- Striver A2Z - DSA/2. patterns/pattern13.cpp
+++ Striver A2Z - DSA/2. patterns/pattern13.cpp
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void get_pattern(int n){
+void get_pattern(int n)
+{
 	int c=1;
-	for(int j=1; j<=n; j++)
+	for(int i=1; i<=n; i++)
 	{
-		for(int k=1; k<=j; k++)
+		for(int j=1; j<=i; j++)
 		{
-			cout << c << " ";
+			cout << c << " ";	
 			c++;
 		}
 		cout << endl;
-	}
-	
+	}	
 }
 
 int main()
